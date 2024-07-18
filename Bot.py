9a154841c9af73ac72b447e78af5d7eee68ea6ec
Nobody <bunnza2020@gmail.com>
2024-07-17 21:47:07 -0500

    Got Stuck

--- Bot.py
+++ Bot.py
@@ -1,11 +1,8 @@
 import praw
 from dotenv import load_dotenv
 import os
-import requests 
-import urllib.parse
 
 load_dotenv("py.env")
-
 
 
 reddit_instance = praw.Reddit(
@@ -18,14 +15,7 @@ reddit_instance = praw.Reddit(
 
 # print(reddit_instance.user.me())
 
-# subreddit = reddit_instance.subreddit("pics")
+subreddit = reddit_instance.subreddit("pics")
 
 # for submission in subreddit.hot(limit=10):
 #     print('Url:', submission.url)
-def Get_Image(reddit_url_code:str):
-    submission = reddit_instance.submission(reddit_url_code)
-    response = requests.get(submission.url)
-    with open(f'Animepics/{submission.author}image.jpg', 'wb') as f:
-        f.write(response.content)
-
-Get_Image("1e5indg")
