0edc4d39cc070461f764e90ce668f5a53a072dac
Nobody <bunnza2020@gmail.com>
2024-07-17 20:26:13 -0500

    More Bot

--- Bot.py
+++ Bot.py
@@ -1,6 +1,9 @@
 import praw
 from dotenv import load_dotenv
 import os
+import requests 
+import urllib.parse
+
 load_dotenv("py.env")
 
 
@@ -8,18 +11,21 @@ load_dotenv("py.env")
 reddit_instance = praw.Reddit(
     client_id= os.getenv("CLIENT_ID"),
     client_secret = os.getenv("CLIENT_SECRET"),
-    username = os.getenv("USERNAME"),
+    username = "Image_IdentifierBot",
     password = os.getenv("PASSWORD"),
     user_agent= os.getenv("USER_AGENT")
     )
 
 # print(reddit_instance.user.me())
 
-subreddit = reddit_instance.subreddit("pics")
+# subreddit = reddit_instance.subreddit("pics")
 
-for submission in subreddit.hot(limit=10):
-    print(submission.title)
-    print('Score:', submission.score)
-    print('Url:', submission.url)
-    print("--------------")
+# for submission in subreddit.hot(limit=10):
+#     print('Url:', submission.url)
+def Get_Image(reddit_url_code:str):
+    submission = reddit_instance.submission(reddit_url_code)
+    response = requests.get(submission.url)
+    with open(f'Animepics/{submission.author}image.jpg', 'wb') as f:
+        f.write(response.content)
 
+Get_Image("1e5indg")
